5a75e24d10be6d2cc67950cca50660dd08dd8c55
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-12-04 08:10:47 +0545

    natural

---
+++ tests/natural.asm
@@ -0,0 +1,31 @@
+; Prints out natural number
+
+; coordinates (middle of the screen)
+ld v0, 30 
+ld v1, 14
+
+; natural number storage
+ld v2, 1
+
+; how many second  to hold, 60 means 1 second
+ld v3, 120
+
+
+loop:
+    ld f, v2
+    cls
+    drw v0,v1,5
+    add v2, 1
+
+    call sleep
+
+    jmp loop
+
+sleep:
+    ld dt, v3
+    loop2:
+        ld v4, dt
+        se v4, 00
+        jmp loop2
+    ret
+
